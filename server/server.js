e85dd3fadf879d6cb23918fbfb6ef421fbf0375c
gmzelias <gmzelias@gmail.com>
2018-05-08 19:37:52 -0400

    Greet new user, and alter users

--- server/server.js
+++ server/server.js
@@ -19,13 +19,22 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text:'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage',{
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('disconnect',()=>{
         console.log('User was disconnected');
     });
 
-    /*socket.on('createEmail', (newEmail)=>{
-        console.log('createEmail', newEmail);
-    });*/
 
         //Listener
     socket.on('createMessage', (message)=>{
@@ -36,13 +45,14 @@ io.on('connection', (socket)=>{
             text:message.text,
             createdAt: new Date().getTime()
         });
-    });
 
-    /*socket.emit('newEmail',{
-        from: 'mike@example.com',
-        text:'how you doing?',
-        createdat:123
-    });*/
+        /*socket.broadcast.emit('newMessage',{
+            from: message.from,
+            text:message.text,
+            createdAt: new Date().getTime()
+        });*/
+
+    });
 
     //socket.emit a event to a single connection
     /*socket.emit('newMessage',{
