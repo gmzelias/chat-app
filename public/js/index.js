39ee22cfc1939e4fdc8fa1c88d219ae9ce291afa
gmzelias <gmzelias@gmail.com>
2018-05-09 13:05:56 -0400

    Add fomr messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -23,4 +23,29 @@ console.log('New email',email);
 
 socket.on('newMessage',function (message) {
     console.log('New message',message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
 });
+
+/*socket.emit('createMessage',{
+    from: 'Elias',
+    text:'Hi hello'
+}, function(data){
+    console.log('Got it!');
+    console.log(data);
+});*/
+
+jQuery('#message-form').on('submit', function(e){
+e.preventDefault();
+socket.emit('createMessage', {
+from: 'User',
+text: jQuery('[name=message]').val()
+}, 
+function(){
+
+});
+
+});
+
