472de4ae2815b785bd84a1fe80f48d8a5f57defa
gmzelias <gmzelias@gmail.com>
2018-05-14 20:05:08 -0400

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,31 @@
 var socket = io();
+
+function scrollToBottom (){
+// Selectors
+var messages = jQuery('#messages');
+var newMessage = messages.children('li:last-child');
+// Heights
+var clientHeight = messages.prop('clientHeight');
+
+var scrollTop = messages.prop('scrollTop');
+
+var scrollHeight = messages.prop('scrollHeight');
+
+var newMessageHeight = newMessage.innerHeight();
+var lastMessageHeight = newMessage.prev().innerHeight();
+
+/*var cai = clientHeight + scrollTop + newMessageHeight + lastMessageHeight;
+var cuid = scrollHeight;
+console.log(lastMessageHeight);
+console.log(newMessageHeight);*/
+
+if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+
+    messages.scrollTop(scrollHeight);
+}
+
+}
+
 socket.on('connect', function(){
     console.log('Connected to server');
 
@@ -30,6 +57,7 @@ var html = Mustache.render(template, {
     createdAt: formattedTime
 });
 jQuery('#messages').append(html);
+scrollToBottom();
 
 
     /* console.log('New message',message);
@@ -56,6 +84,7 @@ socket.on('newLocationMessage',function(message){
     url :message.url
 });
 jQuery('#messages').append(html);
+scrollToBottom();
 });
     
 var messageTextbox = jQuery('[name=message]');
